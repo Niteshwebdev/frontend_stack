3e504ebc312e6809e6780037bb894ddac589ab8a
Niteshwebdev <worldbest178@gmail.com>
2024-07-10 05:15:55 -0700

    Create loading.js

---
+++ src/pages/loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css'
+
+const Loading = ({loading}) => {
+    return (
+        <div>
+            <div class="loading-container">
+              <div class="loader"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Loading;
